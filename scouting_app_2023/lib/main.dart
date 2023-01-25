02ab838e22d024a76423d3c5b9ccd043dd752344
LukeD <ljd223@gmail.com>
2023-01-25 17:20:19 -0500

    changed Wide window

--- scouting_app_2023/lib/main.dart
+++ scouting_app_2023/lib/main.dart
@@ -75,49 +75,49 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  Widget _buildWideContainers() {
+    return Center();
+  }
+
   Widget _buildNormalContainer() {
     return Center(
       child: Container(
         child: Text("Please Turn device Horizontal"),
       ),
-      body: new Container(
-        color: Colors.grey[200],
-        child: new Image.asset('assets/images/field_temp_diagram.png'),
-        alignment: Alignment.center,
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        //   child: Column(
-        //     // Column is also a layout widget. It takes a list of children and
-        //     // arranges them vertically. By default, it sizes itself to fit its
-        //     // children horizontally, and tries to be as tall as its parent.
-        //     //
-        //     // Invoke "debug painting" (press "p" in the console, choose the
-        //     // "Toggle Debug Paint" action from the Flutter Inspector in Android
-        //     // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-        //     // to see the wireframe for each widget.
-        //     //
-        //     // Column has various properties to control how it sizes itself and
-        //     // how it positions its children. Here we use mainAxisAlignment to
-        //     // center the children vertically; the main axis here is the vertical
-        //     // axis because Columns are vertical (the cross axis would be
-        //     // horizontal).
-        //     mainAxisAlignment: MainAxisAlignment.center,
-        //     children: <Widget>[
-        //       const Text(
-        //         'You have clicked the button this many times:',
-        //       ),
-        //       Text(
-        //         '$_counter',
-        //         style: Theme.of(context).textTheme.headline4,
-        //       ),
-        //     ],
-        //   ),
-        // ),
-        // floatingActionButton: FloatingActionButton(
-        //   onPressed: _incrementCounter,
-        //   tooltip: 'Increment',
-        //   child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      // Center is a layout widget. It takes a single child and positions it
+      // in the middle of the parent.
+      //   child: Column(
+      //     // Column is also a layout widget. It takes a list of children and
+      //     // arranges them vertically. By default, it sizes itself to fit its
+      //     // children horizontally, and tries to be as tall as its parent.
+      //     //
+      //     // Invoke "debug painting" (press "p" in the console, choose the
+      //     // "Toggle Debug Paint" action from the Flutter Inspector in Android
+      //     // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+      //     // to see the wireframe for each widget.
+      //     //
+      //     // Column has various properties to control how it sizes itself and
+      //     // how it positions its children. Here we use mainAxisAlignment to
+      //     // center the children vertically; the main axis here is the vertical
+      //     // axis because Columns are vertical (the cross axis would be
+      //     // horizontal).
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       const Text(
+      //         'You have clicked the button this many times:',
+      //       ),
+      //       Text(
+      //         '$_counter',
+      //         style: Theme.of(context).textTheme.headline4,
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add), // Ths trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
