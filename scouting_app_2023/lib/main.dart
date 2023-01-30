1617d4db4aabd84d6af3033dd875a56c4aefc5b8
LukeD <ljd223@gmail.com>
2023-01-30 15:55:12 -0500

    GOATED PUSH

--- scouting_app_2023/lib/main.dart
+++ scouting_app_2023/lib/main.dart
@@ -90,99 +90,305 @@ class _MyHomePageState extends State<MyHomePage> {
                 image: DecorationImage(image: variables.buttonOneImage)),
           ),
           onPressed: () {
-            buttonImageCones();
+            buttonOneImage();
             if (variables.buttonOneState == false) {
+              buttonIndexChanger(1, true);
               variables.buttonOneState = true;
             } else {
+              buttonIndexChanger(1, false);
               variables.buttonOneState = false;
             }
             buttonPressed();
           },
         ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[200],
-          child: const Text('Heed not the rabble'),
+        TextButton(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(image: variables.buttonTwoImage)),
+          ),
+          onPressed: () {
+            buttonTwoImage();
+            if (variables.buttonTwoState == false) {
+              buttonIndexChanger(2, true);
+              variables.buttonTwoState = true;
+            } else {
+              buttonIndexChanger(2, false);
+              variables.buttonTwoState = false;
+            }
+            buttonPressed();
+          },
         ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[300],
-          child: const Text('Sound of screams but the'),
+        TextButton(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(image: variables.button3Image)),
+          ),
+          onPressed: () {
+            button3Image();
+            if (variables.button3State == false) {
+              buttonIndexChanger(3, true);
+              variables.button3State = true;
+            } else {
+              buttonIndexChanger(3, false);
+              variables.button3State = false;
+            }
+            buttonPressed();
+          },
         ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[400],
-          child: const Text('Who scream'),
+        TextButton(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(image: variables.button4Image)),
+          ),
+          onPressed: () {
+            button4Image();
+            if (variables.button4State == false) {
+              buttonIndexChanger(4, true);
+              variables.button4State = true;
+            } else {
+              buttonIndexChanger(4, false);
+              variables.button4State = false;
+            }
+            buttonPressed();
+          },
         ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[500],
-          child: const Text('Revolution is coming...'),
+        TextButton(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(image: variables.button5Image)),
+          ),
+          onPressed: () {
+            button5Image();
+            if (variables.button5State == false) {
+              buttonIndexChanger(5, true);
+              variables.button5State = true;
+            } else {
+              buttonIndexChanger(5, false);
+              variables.button5State = false;
+            }
+            buttonPressed();
+          },
         ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[600],
-          child: const Text('Revolution, they...'),
+        TextButton(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(image: variables.button6Image)),
+          ),
+          onPressed: () {
+            button6Image();
+            if (variables.button6State == false) {
+              buttonIndexChanger(6, true);
+              variables.button6State = true;
+            } else {
+              buttonIndexChanger(6, false);
+              variables.button6State = false;
+            }
+            buttonPressed();
+          },
         ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[100],
-          child: const Text("He'd have you all unravel at the"),
+        TextButton(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(image: variables.button7Image)),
+          ),
+          onPressed: () {
+            button7Image();
+            if (variables.button7State == false) {
+              buttonIndexChanger(7, true);
+              variables.button7State = true;
+            } else {
+              buttonIndexChanger(7, false);
+              variables.button7State = false;
+            }
+            buttonPressed();
+          },
         ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[200],
-          child: const Text('Heed not the rabble'),
+        TextButton(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(image: variables.button8Image)),
+          ),
+          onPressed: () {
+            button8Image();
+            if (variables.button8State == false) {
+              buttonIndexChanger(8, true);
+              variables.button8State = true;
+            } else {
+              buttonIndexChanger(8, false);
+              variables.button8State = false;
+            }
+            buttonPressed();
+          },
         ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[300],
-          child: const Text('Sound of screams but the'),
+        TextButton(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(image: variables.button9Image)),
+          ),
+          onPressed: () {
+            button9Image();
+            if (variables.button9State == false) {
+              buttonIndexChanger(9, true);
+              variables.button9State = true;
+            } else {
+              buttonIndexChanger(9, false);
+              variables.button9State = false;
+            }
+            buttonPressed();
+          },
         ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[400],
-          child: const Text('Who scream'),
+        TextButton(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(image: variables.button10Image)),
+          ),
+          onPressed: () {
+            button10Image();
+            if (variables.button10State == false) {
+              buttonIndexChanger(10, true);
+              variables.button10State = true;
+            } else {
+              buttonIndexChanger(10, false);
+              variables.button10State = false;
+            }
+            buttonPressed();
+          },
         ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[500],
-          child: const Text('Revolution is coming...'),
+        TextButton(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(image: variables.button11Image)),
+          ),
+          onPressed: () {
+            button11Image();
+            if (variables.button11State == false) {
+              buttonIndexChanger(11, true);
+              variables.button11State = true;
+            } else {
+              buttonIndexChanger(11, false);
+              variables.button11State = false;
+            }
+            buttonPressed();
+          },
         ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[600],
-          child: const Text('Revolution, they...'),
+        TextButton(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(image: variables.button12Image)),
+          ),
+          onPressed: () {
+            button12Image();
+            if (variables.button12State == false) {
+              buttonIndexChanger(12, true);
+              variables.button12State = true;
+            } else {
+              buttonIndexChanger(12, false);
+              variables.button12State = false;
+            }
+            buttonPressed();
+          },
         ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[100],
-          child: const Text("He'd have you all unravel at the"),
+        TextButton(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(image: variables.button13Image)),
+          ),
+          onPressed: () {
+            button13Image();
+            if (variables.button13State == false) {
+              buttonIndexChanger(13, true);
+              variables.button13State = true;
+            } else {
+              buttonIndexChanger(13, false);
+              variables.button13State = false;
+            }
+            buttonPressed();
+          },
         ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[200],
-          child: const Text('Heed not the rabble'),
+        TextButton(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(image: variables.button14Image)),
+          ),
+          onPressed: () {
+            button14Image();
+            if (variables.button14State == false) {
+              buttonIndexChanger(14, true);
+              variables.button14State = true;
+            } else {
+              buttonIndexChanger(14, false);
+              variables.button14State = false;
+            }
+            buttonPressed();
+          },
         ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[300],
-          child: const Text('Sound of screams but the'),
+        TextButton(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(image: variables.button15Image)),
+          ),
+          onPressed: () {
+            button15Image();
+            if (variables.button15State == false) {
+              buttonIndexChanger(15, true);
+              variables.button15State = true;
+            } else {
+              buttonIndexChanger(15, false);
+              variables.button15State = false;
+            }
+            buttonPressed();
+          },
         ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[400],
-          child: const Text('Who scream'),
+        TextButton(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(image: variables.button16Image)),
+          ),
+          onPressed: () {
+            button16Image();
+            if (variables.button16State == false) {
+              buttonIndexChanger(16, true);
+              variables.button16State = true;
+            } else {
+              buttonIndexChanger(16, false);
+              variables.button16State = false;
+            }
+            buttonPressed();
+          },
         ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[500],
-          child: const Text('Revolution is coming...'),
+        TextButton(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(image: variables.button17Image)),
+          ),
+          onPressed: () {
+            button17Image();
+            if (variables.button17State == false) {
+              buttonIndexChanger(17, true);
+              variables.button17State = true;
+            } else {
+              buttonIndexChanger(17, false);
+              variables.button17State = false;
+            }
+            buttonPressed();
+          },
         ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[600],
-          child: const Text('Revolution, they...'),
+        TextButton(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(image: variables.button18Image)),
+          ),
+          onPressed: () {
+            button18Image();
+            if (variables.button18State == false) {
+              buttonIndexChanger(18, true);
+              variables.button18State = true;
+            } else {
+              buttonIndexChanger(18, false);
+              variables.button18State = false;
+            }
+            buttonPressed();
+          },
         ),
         Container(
           padding: const EdgeInsets.all(8),
@@ -235,11 +441,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget _buildNormalContainer() {
     double screenWidth = MediaQuery.of(context).size.width;
-    int screenSize = screenWidth.round();
     return Center(
       child: Container(
-        decoration: BoxDecoration(color: Colors.red),
-        child: Text("Please Turn Horizontal"),
+        decoration: const BoxDecoration(color: Colors.red),
+        child: const Text("Please Turn Horizontal"),
       ),
 
       // Center is a layout widget. It takes a single child and positions it
@@ -279,11 +484,190 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-dynamic buttonImageCones() {
+dynamic buttonOneImage() {
   if (variables.buttonOneState == false) {
     variables.buttonOneImage =
-        AssetImage('assets/images/thumbnail_Rodcone.png');
+        const AssetImage('assets/images/thumbnail_rodcone.png');
   } else {
-    variables.buttonOneImage = AssetImage('assets/images/thumbnail_Rod.png');
+    variables.buttonOneImage =
+        const AssetImage('assets/images/thumbnail_rod.png');
+  }
+}
+
+dynamic buttonTwoImage() {
+  if (variables.buttonTwoState == false) {
+    variables.buttonTwoImage =
+        const AssetImage('assets/images/thumbnail_platblock.png');
+  } else {
+    variables.buttonTwoImage =
+        const AssetImage('assets/images/thumbnail_plat.png');
+  }
+}
+
+dynamic button3Image() {
+  if (variables.button3State == false) {
+    variables.button3Image =
+        const AssetImage('assets/images/thumbnail_rodcone.png');
+  } else {
+    variables.button3Image =
+        const AssetImage('assets/images/thumbnail_rod.png');
+  }
+}
+
+dynamic button4Image() {
+  if (variables.button4State == false) {
+    variables.button4Image =
+        const AssetImage('assets/images/thumbnail_rodcone.png');
+  } else {
+    variables.button4Image =
+        const AssetImage('assets/images/thumbnail_rod.png');
+  }
+}
+
+dynamic button5Image() {
+  if (variables.button5State == false) {
+    variables.button5Image =
+        const AssetImage('assets/images/thumbnail_platblock.png');
+  } else {
+    variables.button5Image =
+        const AssetImage('assets/images/thumbnail_plat.png');
+  }
+}
+
+dynamic button6Image() {
+  if (variables.button6State == false) {
+    variables.button6Image =
+        const AssetImage('assets/images/thumbnail_rodcone.png');
+  } else {
+    variables.button6Image =
+        const AssetImage('assets/images/thumbnail_rod.png');
+  }
+}
+
+dynamic button7Image() {
+  if (variables.button7State == false) {
+    variables.button7Image =
+        const AssetImage('assets/images/thumbnail_rodcone.png');
+  } else {
+    variables.button7Image =
+        const AssetImage('assets/images/thumbnail_rod.png');
+  }
+}
+
+dynamic button8Image() {
+  if (variables.button8State == false) {
+    variables.button8Image =
+        const AssetImage('assets/images/thumbnail_platblock.png');
+  } else {
+    variables.button8Image =
+        const AssetImage('assets/images/thumbnail_plat.png');
+  }
+}
+
+dynamic button9Image() {
+  if (variables.button9State == false) {
+    variables.button9Image =
+        const AssetImage('assets/images/thumbnail_rodcone.png');
+  } else {
+    variables.button9Image =
+        const AssetImage('assets/images/thumbnail_rod.png');
+  }
+}
+
+dynamic button10Image() {
+  if (variables.button10State == false) {
+    variables.button10Image =
+        const AssetImage('assets/images/thumbnail_rodcone.png');
+  } else {
+    variables.button10Image =
+        const AssetImage('assets/images/thumbnail_rod.png');
+  }
+}
+
+dynamic button11Image() {
+  if (variables.button11State == false) {
+    variables.button11Image =
+        const AssetImage('assets/images/thumbnail_platblock.png');
+  } else {
+    variables.button11Image =
+        const AssetImage('assets/images/thumbnail_plat.png');
+  }
+}
+
+dynamic button12Image() {
+  if (variables.button12State == false) {
+    variables.button12Image =
+        const AssetImage('assets/images/thumbnail_rodcone.png');
+  } else {
+    variables.button12Image =
+        const AssetImage('assets/images/thumbnail_rod.png');
+  }
+}
+
+dynamic button13Image() {
+  if (variables.button13State == false) {
+    variables.button13Image =
+        const AssetImage('assets/images/thumbnail_rodcone.png');
+  } else {
+    variables.button13Image =
+        const AssetImage('assets/images/thumbnail_rod.png');
+  }
+}
+
+dynamic button14Image() {
+  if (variables.button14State == false) {
+    variables.button14Image =
+        const AssetImage('assets/images/thumbnail_platblock.png');
+  } else {
+    variables.button14Image =
+        const AssetImage('assets/images/thumbnail_plat.png');
+  }
+}
+
+dynamic button15Image() {
+  if (variables.button15State == false) {
+    variables.button15Image =
+        const AssetImage('assets/images/thumbnail_rodcone.png');
+  } else {
+    variables.button15Image =
+        const AssetImage('assets/images/thumbnail_rod.png');
+  }
+}
+
+dynamic button16Image() {
+  if (variables.button16State == false) {
+    variables.button16Image =
+        const AssetImage('assets/images/thumbnail_rodcone.png');
+  } else {
+    variables.button16Image =
+        const AssetImage('assets/images/thumbnail_rod.png');
+  }
+}
+
+dynamic button17Image() {
+  if (variables.button17State == false) {
+    variables.button17Image =
+        const AssetImage('assets/images/thumbnail_platblock.png');
+  } else {
+    variables.button17Image =
+        const AssetImage('assets/images/thumbnail_plat.png');
+  }
+}
+
+dynamic button18Image() {
+  if (variables.button18State == false) {
+    variables.button18Image =
+        const AssetImage('assets/images/thumbnail_rodcone.png');
+  } else {
+    variables.button18Image =
+        const AssetImage('assets/images/thumbnail_rod.png');
+  }
+}
+
+dynamic buttonIndexChanger(buttonNumber, buttonstate) {
+  if (buttonstate) {
+    variables.pageData[buttonNumber] = true;
+  } else {
+    variables.pageData[buttonNumber] = false;
   }
 }
--- scouting_app_2023/lib/variables.dart
+++ scouting_app_2023/lib/variables.dart
@@ -1,7 +1,108 @@
 import 'package:flutter/material.dart';
 
-var rodAlone = const AssetImage('assets/images/thumbnail_Rod.png');
-var rodCone = const AssetImage('assets/images/thumbnail_Rod.png');
+var rodAlone = const AssetImage('assets/images/thumbnail_rod.png');
+var rodCone = const AssetImage('assets/images/thumbnail_rodcone.png');
+var platBlock = const AssetImage('assets/images/thumbnail_platblock.png');
+var platAlone = const AssetImage('assets/images/thumbnail_plat.png');
 
 var buttonOneImage = rodAlone;
 var buttonOneState = false;
+
+var buttonTwoImage = platAlone;
+var buttonTwoState = false;
+
+var button3Image = rodAlone;
+var button3State = false;
+
+var button4Image = rodAlone;
+var button4State = false;
+
+var button5Image = platAlone;
+var button5State = false;
+
+var button6Image = rodAlone;
+var button6State = false;
+
+var button7Image = rodAlone;
+var button7State = false;
+
+var button8Image = platAlone;
+var button8State = false;
+
+var button9Image = rodAlone;
+var button9State = false;
+
+var button10Image = rodAlone;
+var button10State = false;
+
+var button11Image = platAlone;
+var button11State = false;
+
+var button12Image = rodAlone;
+var button12State = false;
+
+var button13Image = rodAlone;
+var button13State = false;
+
+var button14Image = platAlone;
+var button14State = false;
+
+var button15Image = rodAlone;
+var button15State = false;
+
+var button16Image = rodAlone;
+var button16State = false;
+
+var button17Image = platAlone;
+var button17State = false;
+
+var button18Image = rodAlone;
+var button18State = false;
+
+var button19Image = platAlone;
+var button19State = false;
+
+var button20Image = platAlone;
+var button20State = false;
+
+var button21Image = platAlone;
+var button21State = false;
+
+var button22Image = platAlone;
+var button22State = false;
+
+var button23Image = platAlone;
+var button23State = false;
+
+var button24Image = platAlone;
+var button24State = false;
+
+var pageData = {
+  1: false,
+  2: false,
+  3: false,
+  4: false,
+  5: false,
+  6: false,
+  7: false,
+  8: false,
+  9: false,
+  10: false,
+  11: false,
+  12: false,
+  13: false,
+  14: false,
+  15: false,
+  16: false,
+  17: false,
+  18: false,
+  19: false,
+  20: false,
+  21: false,
+  22: false,
+  23: false,
+  24: false,
+  25: false,
+  26: false,
+  27: false,
+};
