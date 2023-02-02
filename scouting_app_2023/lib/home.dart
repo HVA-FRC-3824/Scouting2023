732cbcad24805a948552cb8c3edad5107426e73a
LukeD <ljd223@gmail.com>
2023-02-02 14:54:35 -0500

    Fixed it

--- scouting_app_2023/lib/home.dart
+++ scouting_app_2023/lib/home.dart
@@ -1,17 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:scouting_app_2023/variables.dart' as variables;
-import 'package:scouting_app_2023/main.dart' as dartmain;
-import 'package:scouting_app_2023/grid.dart' as grid;
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-    );
-  }
-}
