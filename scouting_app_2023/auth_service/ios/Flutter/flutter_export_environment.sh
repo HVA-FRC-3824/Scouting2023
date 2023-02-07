ab070823382dbf2bdf76c016b4fef171ef496547
LukeD <ljd223@gmail.com>
2023-02-07 09:43:20 -0500

    Hello

---
+++ scouting_app_2023/auth_service/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/lukedaniel/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/lukedaniel/Documents/Scouting2023/scouting_app_2023/auth_service"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
